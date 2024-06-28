9dcfa5ef436c386b6031b525b66880c7d636d625
Fabricio <fabrix_9017@hotmail.com>
2024-06-27 21:59:03 -0300

    fix PORT

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const path = require ('path');
+const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let PORT = 3000 || process.env.PORT;
+// Ajuste para definir el puerto correctamente
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'));
@@ -18,16 +19,13 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'));
 });
 
-
 // Ruta para manejar la solicitud POST del formulario
 app.post('/submit-form', (req, res) => {
     // Aquí puedes procesar los datos del formulario
     // Luego, redirige a la página index.html
     res.redirect('/');
-  });
+});
 
-
-  app.listen(PORT, () => 
-    console.log("Servidor corriendo" + PORT))
-    
-    
+app.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto " + PORT);
+});
